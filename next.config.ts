4f170a48f31423a11d463d1919945b158b18ac0c
luthfi5372 <luthfi5372@gmail.com>
2026-08-30 18:48:31 +0700

    chore(images): allow new Supabase storage domain in remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -13,8 +13,18 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "afwuyizfsoevcffnhfbk.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
         hostname: "ytpvcjcbqncurujrtart.supabase.co",
         pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+        pathname: "/**",
       },
     ],
   },
